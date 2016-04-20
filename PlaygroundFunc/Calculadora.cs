56ec5f99f75b8164dccda9e7ce127ff7a07d34f9
ciceromarciliano <cicero.oliveira@grupoltm.com.br>
2016-04-20 16:17:12 -0300

    Projeto que tratara de delegate Func

---
+++ PlaygroundFunc/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaygroundFunc
+{
+    public class Calculadora
+    {
+        public int Calcular(int valorA, int valorB, Func<int, int, int> funcao, Predicate<int> validacao = null)
+        {
+            return funcao(valorA, valorB);
+        }
+    }
+}
---
+++ PlaygroundFuncTestes/CalculadoraTestes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaygroundFunc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlaygroundFuncTestes
+{
+    [TestClass]
+    public class CalculadoraTestes
+    {
+        [TestMethod]
+        public void Somar_Dois_Numeros_RetornaTotal()
+        {
+            var total = new Calculadora().Calcular(20, 30, (a, b) => a + b);
+            Assert.AreEqual(50,total);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException), "Tentativa de divisão por zero.")]
+        public void Dividir_Dois_Numeros_Passar_E_Falhar_Divisao_Por_Zero()
+        {
+            var total = new Calculadora().Calcular(100, 0, (a, b) => a/b);
+            Assert.AreEqual(10, total);
+        }
+
+        [TestMethod]
+        public void Dividir_Dois_Numeros_Retornal_Total()
+        {
+            var total = new Calculadora().Calcular(400, 40, (a, b) => a/b, (x) => x > 0);
+            Assert.AreEqual(10, total);
+        }
+    }
+}
